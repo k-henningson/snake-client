f2a03683aa99ab289de59d405320e5917f8c632d
Kyra Henningson <kyrahenningson@hotmail.com>
2022-08-31 21:30:22 +0000

    adding client

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log(`Kyra is connected to the server`);
+  });
+
+  return conn;
+};
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log(`Kyra is connected to the server`);
-  });
-
-  return conn;
-};
+const {connect} = require("./client.js")
 
 console.log("Connecting ...");
 connect();
